e3388068d0f95c2198e24b7c1f2ec64affa846aa
Lamprini <rikoula4@gmail.com>
2024-10-30 20:30:15 +0200

    Third Steps

---
+++ app/src/main/java/com/unipi/konlamp/rallyepulse_app/SignIn.java
@@ -0,0 +1,36 @@
+package com.unipi.konlamp.rallyepulse_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignIn extends AppCompatActivity {
+
+    private EditText editTextText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_in);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+      editTextText = findViewById(R.id.editTextText);
+    }
+    public void enterpassonclick(View view) {
+
+    }
+
+    public void enterspectmodeonclick(View view) {
+
+    }
+}
